56d0bfb81c281d1b691871b5c0046233434c4046
SadLaboka <jitubuh@gmail.com>
2022-02-15 20:24:29 +0300

    Add gendiff script

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+
+    print(args.accumulate(args))
+
+
+if __name__ == '__main__':
+    main()
