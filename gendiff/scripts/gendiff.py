bb165573d2ea79c5499e09fcc5da9510cf34a9aa
SadLaboka <jitubuh@gmail.com>
2022-02-16 23:03:28 +0300

    update gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import argparse
+
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', metavar='FIRST_FILE')
+    parser.add_argument('second_file', metavar='SECOND_FILE')
+    parser.add_argument('-f', '--format', help='set format of output', required=False)
     args = parser.parse_args()
 
     print(args.accumulate(args))
