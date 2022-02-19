dea31b6cb54e4f27779a1a0d53099e7a292479c4
SadLaboka <jitubuh@gmail.com>
2022-02-19 23:02:03 +0300

    add functionality for yaml-files and refactoring

---
+++ tests/fixtures/expected.py
@@ -0,0 +1,9 @@
+"""Expected values for tests"""
+with open('tests/fixtures/file1_file2_json.txt') as file:
+    file1_file2_json = file.read()
+
+with open('tests/fixtures/file1_file1_json.txt') as file:
+    file1_file1_json = file.read()
+
+with open('tests/fixtures/file2_file2_json.txt') as file:
+    file2_file2_json = file.read()
